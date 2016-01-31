ff6619100eb1cdc6e3430f3915210bfe4e8447f6
Raja Pratap Singh <rajapratapsingh@in.ibm.com>
2016-01-31 21:47:42 +0530

    Add project

---
+++ MyGitProject/MyGitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyGitProject
+//
+//  Created by Raja Pratap Singh on 31/01/16.
+//  Copyright © 2016 XYZ India Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
